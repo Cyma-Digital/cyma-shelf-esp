0c909176f20a845086e4bd6933c102d2a8caa2a2
Ryan Soares <ryandehallo229@gmail.com>
2023-01-27 11:39:15 -0300

    StMarche Access Point added

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,31 @@
 #include <WiFi.h>
 
 
-const char* SSID = "";
-const char* password = "";
+const char* SSID = "StMarche";
+const char* password = "12345678";
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  Wifi.begin(SSID, password);
+  Serial.println("\n[+] Creating Access Point...");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(SSID, password);
 
-  While(WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.print()
-  }
+  Serial.print("[+] AP Created with IP Gateway ");
+  Serial.println(WiFi.softAPIP());
+
+  // WiFi.begin(SSID, password);
+
+  // while(WiFi.status() != WL_CONNECTED){
+  //   Serial.println(WiFi.status());
+  //   delay(500);
+  //   Serial.println("Connecting to Wi-FI...");
+  // }
+  // Serial.println(WiFi.status());
+  // Serial.println("Connected to the Wi-FI network...");
+  // Serial.println(WiFi.localIP());
 
 }
 
-void loop() {
-  
-  Serial.println("I can fix it");
-  delay(1000);
-}
+void loop() {}
