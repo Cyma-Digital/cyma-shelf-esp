0905645c2e8f81874ee04ba8ff77cb7c0b73b6c9
snak1n <snak1n@gmail.com>
2023-02-09 16:40:57 -0300

    added fastled support

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "FastLED.h"
 
 #include <WiFiClient.h>
 #include <WebServer.h>
@@ -8,8 +9,11 @@
 
 #include <SPIFFS.h>
 #include <FFat.h>
+#include <ArduinoJson.h>
 
 #define USE_SERIAL Serial
+
+FASTLED_USING_NAMESPACE
 
 void handleRoot();
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length);
@@ -20,19 +24,21 @@ void startConfigWebpage();
 const char* SSID = "StMarche";
 const char* password = NULL;
 
-uint8_t builtinLed = 5;  
-
+#define LED_PIN 22
 
 WiFiClient client;
 WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(9090);
 
+#define NUM_LEDS  50 
+CRGB leds[NUM_LEDS];
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(builtinLed, OUTPUT);
+  pinMode(5, OUTPUT);
   
+  FastLED.addLeds<WS2811, LED_PIN, RGB>(leds, 0, NUM_LEDS); //define
 
   if (!FFat.begin(true)){
     Serial.println("Couldn't mount File System");
@@ -55,23 +61,27 @@ void setup() {
   server.onNotFound(handleNotFound);
   server.begin();
 
-  while (true)
-  {
-    server.handleClient();
-    webSocket.loop();
-  }
+  // while (true)
+  // {
+  //   server.handleClient();
+  //   webSocket.loop();
+  // }
   
 
 }
 
 void loop() {
-  digitalWrite(builtinLed, HIGH);
-  delay(500);
-  digitalWrite(builtinLed, LOW);
-  delay(500);
+
+    server.handleClient();
+    webSocket.loop();
+  
+    FastLED.show();
+
+  // digitalWrite(builtinLed, HIGH);
+  // delay(500);
+  // digitalWrite(builtinLed, LOW);
+  // delay(500);
 }
-
-
 
 void handleRoot() {
 
@@ -88,8 +98,6 @@ void handleNotFound() {
   Serial.println("Handle not Found");
   server.send(404,"text/plain", "404: Not found");
 }
-
-
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length){
 
@@ -108,6 +116,39 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
   case WStype_TEXT:
   {
     USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+
+    const uint8_t size = JSON_OBJECT_SIZE(5);
+    StaticJsonDocument<2048> json;
+    DeserializationError err = deserializeJson(json, payload);
+
+    if (err) {
+        USE_SERIAL.printf("deserializeJson() failed with code ");
+        USE_SERIAL.printf(err.c_str());
+        return;
+    }
+
+
+    int segments = json["segmentsNumber"].as<int>();
+
+    USE_SERIAL.printf("segments: %d\n", segments);
+    USE_SERIAL.printf("segment count: %d\n", json["segments"].size());
+
+    int iIndex = 0;
+
+    for (int i=0; i<json["segments"].size(); i++){
+
+      CRGB segColor = CRGB(json["segments"][i]["color"][0], json["segments"][i]["color"][1], json["segments"][i]["color"][2]);
+      int segmentSize = i == json["segments"].size() - 1 ? 50 : json["segments"][i]["size"];
+
+      for (int p=iIndex; p<segmentSize; p++){
+        leds[p] = segColor;
+      }
+      
+      iIndex = segmentSize;
+
+    }
+
+
   }
   break;
   default:
